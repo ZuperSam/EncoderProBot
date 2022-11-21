e5dfa9740fffe3ad432f69e6d322d69e119e4392
ViaShan <118714573+ViaShan@users.noreply.github.com>
2022-11-21 13:41:58 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,21 +1,20 @@
 from bot.get_cfg import get_config
 class Config(object):
     SESSION_NAME = get_config("SESSION_NAME", "AHCompressorBot")
-    FFMPEG = get_config("FFMPEG")
-    APP_ID = int(get_config("APP_ID", 12345))
-    API_HASH = get_config("API_HASH", "")
-    LOG_CHANNEL = get_config("LOG_CHANNEL")
-    LOGZ = get_config("LOGZ")
+    FFMPEG = get_config("FFMPEG", "ffmpeg -hide_banner -loglevel quiet -progress '''{}''' -i '''{}'''  -i 'https://te.legra.ph/file/e9408e71281cdcb017874.png' -map 0 -filter_complex 'overlay =main_w-(overlay_w+10):main_h-(overlay_h+10)'  -c:v libx265 -crf 27.5 -c:s copy? -s 854x480 -preset veryfast -metadata title='Visit For More Movies [t.me/AniXpo]'  -metadata:s:v title='Visit Website[Anixpo] t.me/AniXpo] - 480p - HEVC - 8bit'  -metadata:s:a title='[Visit t.me/AniXpo] - Opus - 96 kbps' -metadata:s:s title='[AniXpo Substations Alpha]' -c:a libopus -ab 96k -vbr 2 -ac 2 -level 3.1 '''{}''' -y")
+    APP_ID = get_config("APP_ID", "8978848")
+    API_HASH = get_config("API_HASH", "24ce3cff2d32cf529df1c0018e28d6cf")
+    LOG_CHANNEL = get_config("LOG_CHANNEL", "zubEncoderLog")
+    LOGZ = get_config("LOGZ" "zubEncoderLog")
     UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None)
     AUTH_USERS = set(
         int(x) for x in get_config(
-            "AUTH_USERS",
-            should_prompt=True
+            "AUTH_USERS", "1482769753 1995886602 -1001816242004"
         ).split()
     )
-    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "2023958354:AAFezUBfrPUmpDNus6vhVlwUHqvMuxXRXDA")
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
-    BOT_USERNAME = get_config("BOT_USERNAME", "")
+    BOT_USERNAME = get_config("BOT_USERNAME", "X04compress_bot")
     MAX_FILE_SIZE = 2097152000
     TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 2097152000
